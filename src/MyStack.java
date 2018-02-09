e8ac9fb6b0ba6c6ac680f23bd6720edb09183f90
Lucas <ii.fredin@gmail.com>
2018-02-09 11:41:13 +0100

    QueueArray, SinglyLinkedList, MyStack
    created interface myStack for Q10 and implemented it in SinglyLinkedList, havent touched it tho, just added the empty methods

---
+++ src/MyStack.java
@@ -0,0 +1,16 @@
+
+public interface MyStack {
+
+	public void push(int i);
+	
+	public void pop();
+	
+	public boolean isEmpty();
+	
+	public void makeEmpty();
+	
+	public void top();
+	
+	public void topNPop(); 
+	
+}
--- src/QueueArray.java
+++ src/QueueArray.java
@@ -5,7 +5,7 @@ class QueueArray<Item> {
   private Item[] arr;
   //i added these
   private int front = -1;
-  private int back = -1;
+  private int back = -1; //do we need a back or just use size() for this??
   
   // We need this pragma to avoid warnings triggerred by
   // the unsafe cast to Item[].
@@ -18,10 +18,15 @@ class QueueArray<Item> {
 	    QueueArray<Integer> q = new QueueArray<Integer>();
 	    q.enqueue(4);
 	    q.enqueue(5);
-	    System.out.println(q.getFront());
-	    q.dequeue();
-	    q.enqueue(2);
-	    System.out.println(q.getFront());
+	    q.enqueue(5);
+	    q.enqueue(5);
+	    q.enqueue(5);
+	   // q.enqueue(5);
+	    //System.out.println(q.getFront());
+	   // q.dequeue();
+	   // q.enqueue(2);
+	   // System.out.println(q.getFront());
+	   System.out.println(q.toString());
 	  }
 
   public int size() {
@@ -31,22 +36,29 @@ class QueueArray<Item> {
   public void resize() {
 	  //need to cast the object as an item to be able to do this
 	  Item[] temp = (Item [])new Object[initial_size*2]; //why cant we just use ints lol
-  
+	  System.out.println("in resize");
 	  for(int i = 0; i < size; i++) {
-		  temp[i] = arr[(front + i) % size];
+		  temp[i] = arr[(front + i) % size()];
 	  }
 	  
 	  arr = temp;
 	  front = 0;
+	  back = size(); //dont need i think
 	  
   }
   
   public void enqueue(Item x) {
-	  if(size == 0) {
-		  
+	 System.out.println("back before adding new"+back);
+	  if(initial_size == size) { //??
+		  System.out.println("entered resize");
+		  resize();
 	  }
-
-    throw new UnsupportedOperationException();
+	  if(front == -1) front = 0; //change this maybe
+	  back++;
+	  arr[back] = x;
+	  size++;
+	  System.out.println("back after adding new" + back);
+ //   throw new UnsupportedOperationException();
   }
 
   public void dequeue() {
@@ -66,17 +78,17 @@ class QueueArray<Item> {
   }
 
   // Represent the contents of the list as a String
-  /*
+ 
   public String toString() {
     StringBuilder res = new StringBuilder("{");
     if (size > 0) {
-      res.append(firstEl.toString());
+      res.append(arr[front].toString());
       for (int i = 1; i < size; ++i) {
         res.append(", ");
-        res.append(el.toString());
+        res.append(arr[i].toString());
       }
     }
     res.append("}");
     return res.toString();
-  }*/
+  }
 }
